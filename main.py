c9532a4010d302959e595d46dcbcb89d4ee17b5a
kaiquejw <kaiquecapt@gmail.com>
2026-07-23 19:21:39 -0300

    prod miriam

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ CONTAS = [
     {
         "nome": "Miriam",
         "secret_name": "SESSION_MIRIAM",
-        "chat_id": -4992766314,
+        "chat_id": -1002459968766,
         "msg": "Miria/adriano raio 3"
     },
 ]
 
 # ⚠️ AJUSTE AQUI PARA O DIA DA SENHA ⚠️
-HORA_ALVO = 19
-MINUTO_ALVO = 20
+HORA_ALVO = 20
+MINUTO_ALVO = 30
 
 async def sniper_individual(conta, alvo):
     """Função otimizada para VELOCIDADE MÁXIMA (Modo Turbo)"""
